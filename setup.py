055081077f0f84472c9b43726eeff0d0d5061ca8
nommu-moose <moose@nommumail.co.uk>
2025-10-24 21:03:01 +0200

    v0.1.12 fixing wheel

--- setup.py
+++ setup.py
@@ -3,19 +3,14 @@ from Cython.Build import cythonize
 import os
 
 # Recursively find all .py files in the 'src' directory, excluding __init__.py
-
 def find_py_files(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".py") and file != "__init__.py":
                 yield os.path.join(root, file)
 
-# Detect if this source tree is already precompiled/obfuscated
-PRECOMPILED_MARKER = os.path.join("src", "monpy", "_precompiled.marker")
-is_precompiled = os.path.exists(PRECOMPILED_MARKER)
-
-# Create a list of Extension modules for Cython unless precompiled
-source_files = list(find_py_files("src")) if not is_precompiled else []
+# Create a list of Extension modules for Cython
+source_files = list(find_py_files("src"))
 extensions = [
     Extension(
         name=os.path.splitext(os.path.relpath(path, "src"))[0].replace(os.path.sep, "."),
@@ -30,6 +25,6 @@ setup(
         compiler_directives={"language_level": "3"},
         exclude_failures=True,
         # Use build_dir to keep intermediate C files out of the source tree
-        build_dir="build/cython_build",
-    ) if extensions else [],
+        build_dir="build/cython_build" 
+    ),
 )
--- src/monpy/__init__.py
+++ src/monpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 from .client import MondayClient
 from .exceptions import MondayAPIError
